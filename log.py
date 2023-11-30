d7b5f0efb801e1ecd344728dde8e2b2c4e84c9ea
Kinse <22683778+sofoxe1@users.noreply.github.com>
2023-11-30 02:12:35 +0100

    stuff

--- log.py
+++ log.py
@@ -5,7 +5,7 @@ import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path',help='stellaris log path',default=f"{path.expanduser('~')}/.var/app/com.valvesoftware.Steam/.local/share/Paradox Interactive/Stellaris/crashes/")
-parser.add_argument('-d', '--depth', help='stack trace depth, 0 to disable ',default=1,type=int) #i recomend leaving it at one otherwise some errors while be double while other not
+parser.add_argument('-d', '--depth', help='stack trace depth, 0 to disable ',default=1,type=int) 
 args = parser.parse_args()
 
 lines=[]
@@ -25,16 +25,16 @@ crash_c = 0
 
 def error_log(fline,_depth,r):
     global crash_c
-    if "::" in fline or ".so" in fline: # dirty bugfix for element w/o a name
-        pass
+    if "::" in fline or ".so" in fline: pass # lazy bugfix for element w/o a name
     else:
         return _depth, r
     
     
     _depth += 1
-    if not re.search('|'.join(["CPdxCrashReporter","libstdc++.so.6","libc.so.6", "libpops_api.so","CString::CString","std"]), line):
-        
+    
+    if not re.search('|'.join(["libstdc++.so.6","libc.so.6", "libpops_api.so","std"]), line):
         crash_c +=1
+
         if fline not in errors:
             errors.append(fline)
             errors_c.append(1)
@@ -48,36 +48,28 @@ def error_log(fline,_depth,r):
     return _depth, r
 
 for line in lines:
-    
-    if "Demangled Stack Trace:" in line:
-        r = True
-    elif "Original Stack Trace:" in line:
-        r = False
+    if line[0:21] == "Original Stack Trace:": r = False
+    elif line[0:22] == "Demangled Stack Trace:": r = True
     
     
     elif r:
-        
-        if "./stellaris" in line:
+        if line[2:13] == "./stellaris":
             fline=line.split(" ")[22:]
-            # print(line)
-            if fline[0] != "(":
-                raise Exception(f"string formating is incorrect for {line}")
-            fline.pop(0)
-            if fline[0] in ["main", "_start","RunGame(int,"]:
-                continue
-            fline=fline[0]
-            _depth,r = error_log(fline,_depth,r)
-        elif ".so" in line:
+            if fline[0] != "(": raise Exception(f"string formating is incorrect for: {line}")
+            fline=fline[1]
             
+            _depth,r = error_log(fline,_depth,r)
+
+
+        elif ".so" in line:
             fline=line.split(" ")[2:][0].split("/")[-1]
-            if ".so" not in fline:
-                raise Exception(f"string formating is incorrect for")
+            if ".so" not in fline: raise Exception(f"string formating is incorrect for: {line}")
+
             _depth,r = error_log(fline,_depth,r)
                 
             
             
 for i in range(len(errors)): 
-    # print(errors_c[i])
     errors[i]=f"{errors_c[i]} {errors[i]}  {round(errors_c[i]/crash_c*100,1)}%"
    
 print("\n".join(sorted(errors, reverse=True)))
